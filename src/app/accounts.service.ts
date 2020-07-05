94b670399677886abdea25a1be4678bfc0d79f43
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-07-04 18:15:10 -0600

    inject accounts.service with logging.service
    add LoggingService to app.module as a provider;
    remove LoggingService from components;
    inject LoggingService into AccountsService to log server status when each of AccountsService's methods are called

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,4 +1,11 @@
+import { Injectable } from '@angular/core';
+
+import { LoggingService } from './logging.service';
+
+@Injectable()
+
 export class AccountsService {
+  
   accounts = [
     {
       name: 'Master Account',
@@ -13,12 +20,16 @@ export class AccountsService {
       status: 'unknown'
     }
   ];
+  
+  constructor(private loggingService: LoggingService) {}
 
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status:string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
